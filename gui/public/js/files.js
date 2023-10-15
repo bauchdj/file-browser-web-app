4e545fef2893a6af90c389faf30ed169e2716a0a
David Bauch <jamesbauch@gmail.com>
2023-10-14 20:41:02 -0600

    Added directory changing

--- gui/public/js/files.js
+++ gui/public/js/files.js
@@ -1,3 +1,54 @@
+const userDirectory = "james";
+let currentPath = userDirectory;
+
+function updateDirectoryButtons(path) {
+	const container = document.querySelector("body > div > div.files > div > div.flex-r > div:last-child");
+	const newContainer = $(container.outerHTML);
+	newContainer.empty();
+
+	let steppingPath = '';
+	const buttonList = path.split('/');
+	buttonList.forEach( elText => {
+		const button = document.createElement('button');
+		button.textContent = elText;
+		button.className = "btn";
+		steppingPath += elText + '/';
+		let jumpToPath = steppingPath;
+		button.onclick = function (e) {
+			openDirectory(jumpToPath);
+		}
+		const div = document.createElement('div');
+		div.textContent = '/';
+		div.className = 'center-y';
+		newContainer.get(0).appendChild(div);
+		newContainer.get(0).appendChild(button);
+	});
+
+	const fileOptions = document.querySelector("body > div > div.files > div > div.flex-r");
+	fileOptions.removeChild(container);
+	fileOptions.appendChild(newContainer.get(0));
+}
+
+function updateInputPathText(path) {
+	// const path = (currentPath === "") ? userDirectory + '/' + directoryName : currentPath + '/' + directoryName; 
+	currentPath = path;
+	const input = document.querySelector("body > div > div.files > div > div.flex-1 > input");
+	input.value = path;
+}
+
+function openDirectory(path) {
+	path = path.replace(/\/$/, '');
+	console.log(path);
+	updateInputPathText(path);
+	updateDirectoryButtons(path);
+	ajaxPostFileListRequest(path);
+}
+
+function openFile(path) {
+	const filename = path.split('/').pop();
+	console.log('Tried to open ' + filename);
+}
+
 function utcToCurrentTime(utcTimeString) {
 	const utcDate = new Date(utcTimeString);
 	const localDate = new Date(utcDate.localTime());
@@ -8,7 +59,7 @@ function utcToCurrentTime(utcTimeString) {
 function addFilesToTable(fileList) {
 	const tableBody = document.querySelector("body > div > div.files > table > tbody.table-group-divider");
 	const body = $(tableBody.outerHTML);
-	body.html();
+	body.empty();
 
 	fileList.forEach( fileStats => {
 		const fileType = fileStats.isDirectory ? "Folder" : fileStats.fileExtension;
@@ -24,9 +75,23 @@ function addFilesToTable(fileList) {
 			fileStats.sizeInBytes,
 			fileType,
 		];
-		els.forEach( e => {
+
+		function setElOnclick(el, func) {
+			el.onclick = function (e) {
+				func(currentPath + '/' + fileStats.filename);
+			}
+		}
+
+		els.forEach( item => {
 			const tableData = document.createElement('td');
-			tableData.innerHTML = e;
+			if ( item === fileStats.filename ) {
+				if ( fileStats.isDirectory ) {
+					setElOnclick(tableData, openDirectory);
+				} else {
+					setElOnclick(tableData, openFile);
+				}
+			}
+			tableData.innerHTML = item;
 			tableRow.appendChild(tableData);
 		});
 		body.get(0).appendChild(tableRow);
@@ -38,14 +103,12 @@ function addFilesToTable(fileList) {
 	table.appendChild(body.get(0));
 }
 
-const userDirectory = "james";
-
-$(document).ready(function() {
+function ajaxPostFileListRequest(path) {
 	$.ajax({
 		url: '/files',
 		method: 'POST',
 		dataType: 'json',
-		data: { user: userDirectory },
+		data: { user: path },
 		success: function(data) {
 			addFilesToTable(data);
 		},
@@ -53,4 +116,8 @@ $(document).ready(function() {
 			console.error("Couldn't get files");
 		}
 	});
+}
+
+$(document).ready(function() {
+	ajaxPostFileListRequest(userDirectory);
 });
