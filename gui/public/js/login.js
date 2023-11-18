f7bcdeb663ece76613aa3d25825063bc64458370
David Bauch <jamesbauch@gmail.com>
2023-11-17 22:56:39 -0700

    New login scripts

---
+++ gui/public/js/login.js
@@ -0,0 +1,62 @@
+function handleFormSubmit(event) {
+	event.preventDefault(); // Prevent default form submission
+
+	const form = event.target;
+	const username = form.querySelector('input[name="username"]').value;
+	const password = form.querySelector('input[name="password"]').value;
+	const isCreateUser = form.querySelector('#createUserCheckbox').checked;
+
+	if (isCreateUser) {
+		createUser(username, password);
+	} else {
+		login(username, password);
+	}
+}
+
+function postData(url, data, successCallback, errorCallback) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', url, true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				successCallback(JSON.parse(xhr.responseText));
+			} else {
+				errorCallback(xhr.statusText);
+			}
+		}
+	};
+
+	xhr.onerror = () => {
+		errorCallback('Network error');
+	};
+
+	xhr.send(JSON.stringify(data));
+}
+
+function onLogin(username) {
+	localStorage.setItem('user', username)
+	window.location.href = "/home";
+}
+
+function createUser(username, password) {
+	postData('/create/user', { username, password }, data => {
+		console.log('User created:', data);
+		onLogin(username);
+	}, err => {
+		console.error('Error:', err);
+		alert("User already exists");
+	});
+}
+
+function login(username, password) {
+	postData('/login', { username, password }, res => {
+		console.log('Login successful:', res);
+		onLogin(username);
+	}, err => {
+		console.error('Error:', err);
+		alert("Invalid credentials");
+	});
+}
+
---
+++ gui/public/js/logout.js
@@ -0,0 +1,23 @@
+function logout(event) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', '/logout', true);
+
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status >= 200 && xhr.status < 300) {
+				console.log('Logout successful');
+				localStorage.removeItem('user');
+				window.location.href = '/login';
+			} else {
+				console.error('Logout failed:', xhr.statusText);
+			}
+		}
+	};
+
+	xhr.onerror = () => {
+		console.error('Network error during logout');
+	};
+
+	xhr.send();
+}
+
