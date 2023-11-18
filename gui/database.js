6e746adca4ee81f26b3e6ce8d38bc56f8e0f88d4
David Bauch <jamesbauch@gmail.com>
2023-11-17 22:37:41 -0700

    Login, create user, log out all work

--- gui/database.js
+++ gui/database.js
@@ -1,9 +1,25 @@
 const bcrypt = require('bcrypt');
+const fs = require('fs-extra');
+const path = require('path');
+const crypto = require('crypto');
 const { MongoClient } = require("mongodb");
 const config = require("./db-config.json");
 
 const uri = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
 const client = new MongoClient(uri);
+const db = client.db("Cluster0");
+const collection = db.collection("users");
+
+const sessionIdHash = {};
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+	await client.connect();
+	await db.command({ ping: 1 });
+})().catch((ex) => {
+	console.log(`Unable to connect to database with ${uri} because ${ex.message}`);
+	process.exit(1);
+});
 
 async function hashPassword(password) {
 	const saltRounds = 10; // This is a cost factor for hashing
@@ -17,58 +33,131 @@ async function checkPassword(inputPassword, storedHashedPassword) {
 
 async function findUserByUsername(username) {
 	try {
-		await client.connect();
-		const db = client.db("Cluster0");
-		const collection = db.collection("users");
 		const user = await collection.findOne({ username: username });
 		return user;
 	} catch (err) {
 		console.error("Error finding user:", err);
-		throw err; // Re-throw the error to be caught by the caller
+		return false;
 	}
 }
 
 async function checkUserLogin(username, password) {
 	try {
 		const user = await findUserByUsername(username);
-		if (user) {
+		if (user && user.password) {
 			const isMatch = await checkPassword(password, user.password);
-			
 			if (isMatch) {
-				//return { success: true, userId: user._id };
 				return { success: true };
-			} else {
-				return { success: false };
 			}
 		}
+
+		return { success: false };
 	} catch (err) {
 		console.error("Error checking user login:", err);
-		//throw err; // Re-throw the error to be caught by the caller
+		return false;
+	}
+}
+
+async function removeSessionId(username, sessionId) {
+	try {
+		delete sessionIdHash[sessionId];
+		await collection.updateOne({ username: username }, { $unset: { sessionId: "" } });
+		console.log(`Session ID for ${username} has been cleared`);
+		return true;
+	} catch (err) {
+		console.error("Error removing user session:", err);
+		return false;
+	}
+}
+
+async function clearSessionId(username, sessionId) {
+	clearTimeout(sessionIdHash[sessionId]);
+	return await removeSessionId(username, sessionId);
+}
+
+async function updateSessionId(username, sessionIdLifetime, sessionId) {
+	try {
+		if (sessionId === undefined) {
+			function generateNewSessionId() {
+				return crypto.randomBytes(16).toString('hex');
+			}
+			sessionId = generateNewSessionId();
+			const result = await collection.updateOne({ username: username }, { $set: { sessionId: sessionId } });
+		}
+
+		sessionIdHash[sessionId] = setTimeout(async () => {
+			await removeSessionId(username, sessionId);
+		}, sessionIdLifetime);
+
+		return sessionId;
+	} catch (err) {
+		console.error("Error updating user session:", err);
+		return false;
+	}
+}
+
+async function checkSessionId(username, sessionId) {
+	try {
+		if (sessionIdHash[sessionId]) {
+			return true;
+		}
+
+		const user = await collection.findOne({ username: username, sessionId: sessionId } );
+
+		if (user && user.sessionId) {
+			return true;
+		}
+
+		return false;
+	} catch (err) {
+		console.error("Error checking user session:", err);
+		return false;
+	}
+}
+
+async function userPathExists(path) {
+	try {
+		await fs.access(path, fs.constants.F_OK);
+		return true;
+	} catch (err) {
+		return false;
 	}
 }
 
 async function createUser(username, password) {
+	const basePath = path.resolve(__dirname + "/../") + "/"; // Turns relative path to absolute path. Express relative path is malicious
+	const userPath = basePath + "users/" + username + "/";
+	if (await userPathExists(userPath)) return false;
+
+	const userExists = await findUserByUsername(username);
+	if (userExists) return false;
+
 	try {
+		await fs.mkdir(userPath);
+
 		const pwd = await hashPassword(password);
-		await client.connect();
-		const db = client.db("Cluster0");
-		const collection = db.collection("users");
 		const document = { username: username, password: pwd };
 		await collection.insertOne(document);
+
+		return true;
 	} catch (err) {
 		console.error("Error creating user", err);
-		// throw err;
 	}
-}
 
-module.exports = {
-	createUser,
-	checkUserLogin
-};
+	return false;
+}
 
 process.on("SIGINT", async () => {
 	await client.close();
-	console.log("MongoDB client disconnected on app termination");
+	console.log("\nMongoDB client disconnected on app termination");
 	process.exit(0);
 });
+
+module.exports = {
+	createUser,
+	checkUserLogin,
+	updateSessionId,
+	checkSessionId,
+	clearSessionId,
+};
 
--- gui/index.js
+++ gui/index.js
@@ -14,43 +14,91 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // Text to display for the service name
 const serviceName = process.argv.length > 3 ? process.argv[3] : 'file-browser-web-app';
 
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
 // Default files accessible without login
 app.use('/css/main.css', express.static(__dirname + '/public/css/main.css'));
 app.use('/css/login.css', express.static(__dirname + '/public/css/login.css'));
+app.use('/js/login.js', express.static(__dirname + '/public/js/login.js'));
 app.use('/images/file-browser-icon.png', express.static(__dirname + '/public/images/file-browser-icon.png'));
 app.use('/favicon.ico', express.static(__dirname + '/public/favicon.ico'));
 
-app.get('/', (req, res) => {
+app.get(['/', '/login'], (req, res) => {
 	res.sendFile('login.html', { root: __dirname + '/public' });
 });
 
-app.post('/login', async function (req, res) {
+async function setAuthCookie(user, res, sessionId) {
+	const maxAge = 3600000;
+	sessionId = await db.updateSessionId(user, maxAge, sessionId);
+
+	res.cookie('user', user, { secure: true, httpOnly: true, sameSite: 'strict', maxAge: maxAge });
+	res.cookie('sessionId', sessionId, { secure: true, httpOnly: true, sameSite: 'strict', maxAge: maxAge });
+}
+
+app.post('/create/user', async (req, res) => {
+	const username = req.body.username;
+	const pwd = req.body.password;
+
+	// Idea: require patten at end of password for future security
+	// if (pwd.match(/regex expr/) cont... else NOPE)
+
+	const userCreated = await db.createUser(username, pwd);
+
+	if (userCreated) {
+		await setAuthCookie(username, res);
+		res.json({ success: true, message: 'User creation successful' });
+	} else {
+		res.status(409).send({ success: false, message: 'Existing user' });
+	}
+});
+
+app.post('/login', async (req, res) => {
 	const username = req.body.username;
 	const pwd = req.body.password;
 
 	const promise = await db.checkUserLogin(username, pwd);
 
 	if (promise.success) {
-		//const sessionId = generateNewSessionId();
-		//res.cookie('sessionId', sessionId, { secure: true, httpOnly: true, maxAge: 3600000, sameSite: 'strict' });
-		//db.addSessionId(sessionId); // Add this function to database.js
-		res.cookie('isLoggedIn', 'true', { secure: true, httpOnly: true, maxAge: 3600000, sameSite: 'strict' });
-		res.redirect('/home');
+		await setAuthCookie(username, res);
+		res.json({ success: true, message: 'Login successful' });
 	} else {
-		res.redirect('/');
+		res.status(401).send({ success: false, message: 'Invalid credentials' });
 	}
 });
 
-function checkAuth(req, res, next) {
-	//const sessionId = req.cookies.sessionId;
-	//if (sessionId && isValidSession(sessionId)) {
-	if (req.cookies.isLoggedIn === 'true') {
-		res.cookie('isLoggedIn', 'true', { secure: true, httpOnly: true, maxAge: 3600000, sameSite: 'strict' });
-		next();
-	} else {
-		res.redirect('/');
+
+async function checkAuth(req, res, next) {
+	const user = req.cookies.user;
+	const sessionId = req.cookies.sessionId;
+
+	if (sessionId) {
+		const sessionExists = await db.checkSessionId(user, sessionId);
+		if (sessionExists) {
+			await setAuthCookie(user, res, sessionId);
+			next();
+			return;
+		}
 	}
+
+	res.redirect('/');
 }
+
+app.post('/logout', checkAuth, async (req, res) => {
+	const user = req.cookies.user;
+	const sessionId = req.cookies.sessionId;
+
+	const cleared = await db.clearSessionId(user, sessionId);
+
+	if (cleared) {
+		res.clearCookie('user');
+		res.clearCookie('sessionId');
+		res.json({ success: true, message: 'Logout successful' });
+		return;
+	}
+
+	res.status(500).json({ success: false, message: 'Logout failed' });
+});
 
 app.get('/:type(css|js|images)/:file', checkAuth, (req, res) => {
 	const type = req.params.type;
